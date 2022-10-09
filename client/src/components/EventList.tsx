683302e425358d67d4831513c1ad4b0ee48a7b5a
Marcus Lee <102197705+leh-tech@users.noreply.github.com>
2022-10-09 17:51:03 -0400

    Added redux hooks and listgroup to eventlist

--- client/src/components/EventList.tsx
+++ client/src/components/EventList.tsx
@@ -1,7 +1,23 @@
+import { useAppSelector, useAppDispatch } from "helpers/hooks"
+import { ListGroup } from "react-bootstrap"
 import Sort from "./Sort"
 
 const EventList = (props) => {
-    return <Sort by="createdAt">
+    const count = useAppSelector((state) => state.events.events);
+    const dispatch = useAppDispatch();
+    const eventClicked = () => {
+        alert('You clicked the third ListGroupItem');
+    };
 
-    </Sort>
+    return <ListGroup defaultActiveKey="#link1">
+        <ListGroup.Item action href="#link1">
+            Link 1
+        </ListGroup.Item>
+        <ListGroup.Item action href="#link2" disabled>
+            Link 2
+        </ListGroup.Item>
+        <ListGroup.Item action onClick={eventClicked}>
+            This one is a button
+        </ListGroup.Item>
+    </ListGroup>
 }
